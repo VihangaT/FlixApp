b81eb741af28518a17524759ba0c1fc5ff6e143b
vihanga <liyanage.vihanga99@gmail.com>
2021-07-24 00:45:09 +0530

    homePageImplemented

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -27,7 +27,7 @@ export default function Featured({ type }) {
             </div>
         )
             }
-        <img width="100%" src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" alt=""/>
+        <img width="100%" src="https://wallpapercave.com/wp/wp3212730.jpg" alt=""/>
             <div className="info">
                 <img src="https://occ-0-1432-1433.1.nflxso.net/dnm/api/v6/LmEnxtiAuzezXBjYXPuDgfZ4zZQ/AAAABUZdeG1DrMstq-YKHZ-dA-cx2uQN_YbCYx7RABDk0y7F8ZK6nzgCz4bp5qJVgMizPbVpIvXrd4xMBQAuNe0xmuW2WjoeGMDn1cFO.webp?r=df1" alt="" />
                 <span className="desc">
