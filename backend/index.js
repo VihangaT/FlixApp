b0c570665e7a2021ec2d4b9bdb48b74dc908dbe1
vihanga <liyanage.vihanga99@gmail.com>
2021-08-04 01:34:39 +0530

    user update implemented

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const dotenv= require("dotenv");
 const authRouter= require("./routes/auth");
+const userRouter= require("./routes/users")
 
 dotenv.config();
 
@@ -18,6 +19,7 @@ mongoose.connect(process.env.MONGO_URL, {
 app.use(express.json());
 
 app.use("/api/auth",authRouter);
+app.use("/api/users",userRouter);
 
 app.listen(process.env.PORT, () => {
   console.log("Backend Server is Running");
--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,29 +1,39 @@
 const router = require("express").Router();
 const User = require("../models/User");
 const CryptoJS = require("crypto-js");
-
+const verify = require("../verifyToken")
 //update
-router.put("/:id", async (req, res) => {
-  if (req.user.id === req.params.id || req.user.isAdmin) {
-    if (req.body.password) {
-      req.body.password = CryptoJS.AES.decrypt(
-        req.body.password,
-        process.env.SECRET_KEY
-      ).toString;
+router.put("/:id", verify,  async(req, res) => {
+    // console.log("req",req.body)
+    console.log("res",res)
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      if (req.body.password) {
+        req.body.password = CryptoJS.AES.encrypt(
+          req.body.password,
+          process.env.SECRET_KEY
+        ).toString();
+      }
+  
+      try {
+        const updatedUser =  await User.findByIdAndUpdate(
+          req.params.id,
+          {
+            $set: req.body,
+          },
+          { new: true }
+        );
+        res.status(200).json(updatedUser);
+      } catch (err) {
+        res.status(500).json(err);
+      }
+    } else {
+      res.status(403).json("You can update only your account!");
     }
-    try {
-      const updateUser = await User.findByIdAndUpdate(req.params.id, {
-        $set: req.body,
-      });
-      res.status(200).json(updateUser)
-    } catch (error) {
-      res.status(500).json(err);
-    }
-  } else {
-    res.status.toString(403).json("You can update only your account!");
-  }
-});
+  });
 //delete
 //get
 //getAll
 //get user status
+
+
+module.exports = router;
