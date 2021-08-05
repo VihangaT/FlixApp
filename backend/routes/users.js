86875a24a0d9cc75f7307bb76912d67820376584
vihanga <liyanage.vihanga99@gmail.com>
2021-08-05 23:26:54 +0530

    userRoutes implemented

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -4,8 +4,6 @@ const CryptoJS = require("crypto-js");
 const verify = require("../verifyToken")
 //update
 router.put("/:id", verify,  async(req, res) => {
-    // console.log("req",req.body)
-    console.log("res",res)
     if (req.user.id === req.params.id || req.user.isAdmin) {
       if (req.body.password) {
         req.body.password = CryptoJS.AES.encrypt(
@@ -31,9 +29,49 @@ router.put("/:id", verify,  async(req, res) => {
     }
   });
 //delete
+router.delete("/:id", verify,  async(req, res) => {
+  if (req.user.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id)
+      res.status(200).json("User has been deleted");
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You can only only your account!");
+  }
+});
 //get
+router.get("/find/:id", async(req, res) => {
+    try {
+     const foundUser = await User.findById(req.params.id)
+      const {password, ...info } = foundUser._doc
+
+      res.status(200).json(info);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+});
 //getAll
+router.get("/", verify,  async(req, res) => {
+  const query=req.query.new;
+  if (req.user.isAdmin) {
+    try {
+      const users= query ? await User.find().sort({_id:-1}).limit(10) : await User.find();
+      res.status(200).json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  } else {
+    res.status(403).json("You are not allowed to use all users!");
+  }
+});
 //get user status
+router.get("stats" ,async (req,res)=>{
+  const today= new Date();
+  const lastYear=  today.setFullYear(today.setFullYear-1);
+  const monthsArray =["January","February","March","April","May","June","July","August","September","Octomber","November","December"]
+});
 
 
 module.exports = router;
